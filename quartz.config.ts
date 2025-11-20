12819924bf7fa31e1d4ad30c47ffc10b15a19d21
Кирилл Назаров <85652843+NazarovKI@users.noreply.github.com>
2025-11-20 16:50:31 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "ООО Мастер",
+    pageTitleSuffix: "ул. Раздольная д. 34, Екатеринбург",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
